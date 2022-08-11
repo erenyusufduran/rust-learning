e65d6266a0b13c8dce5d1b420a57fa8012efa935
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-08-12 01:03:25 +0300

    guessing game, variables, data types

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\eren\OneDrive\Belgeler\rust\projects\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\eren\OneDrive\Belgeler\rust\projects\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\eren\OneDrive\Belgeler\rust\projects\variables\target\debug\variables.exe: C:\Users\eren\OneDrive\Belgeler\rust\projects\variables\src\main.rs
