d2c8077ed5f1e2b6283a8ff593e76435d5bd2466
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-08-18 18:44:10 +0300

    6.3 - if let

--- matches/src/main.rs
+++ matches/src/main.rs
@@ -24,6 +24,29 @@ fn main() {
         3 => add_fancy_hat(),
         7 => remove_fancy_hat(),
         _ => (), // or other => erelkdflsakd(other) // _ =>
+    };
+
+    // if let
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {}", max),
+        _ => (),
+    };
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+
+    let coin = Coin::Quarter(UsState::Alabama);
+
+    let mut count = 0;
+    // match coin {
+    //     Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+    //     _ => count += 1,
+    // }
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
     }
 }
 
